7c04db079f50a556ed0e46c48aab6e56989546c6
andrei <andrei@mageda.com.br>
2020-07-14 22:06:20 -0300

    update component name

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { Navigation } from 'react-native-navigation';
 import { startTabNavigation } from './src/utils/navigation';
-import App from './App';
 
-Navigation.registerComponent('brain.counters', () => App);
+import Counter from './src/screens/Counters';
+
+Navigation.registerComponent('brain.counters', () => Counter);
 
 Navigation.events().registerAppLaunchedListener(() => {
   startTabNavigation();
