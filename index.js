529bcc82533934006bec114153c54dd942addff8
andrei <andrei@mageda.com.br>
2020-07-14 22:03:04 -0300

    convert root component into compatible navigation format

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { Navigation } from 'react-native-navigation';
+import { startTabNavigation } from './src/utils/navigation';
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('brain.counters', () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  startTabNavigation();
+});
