6b06cac473bddcf0a15714a2548c369be81b95ea
andrei <andrei@mageda.com.br>
2020-07-14 22:06:10 -0300

    simplify file structure

---
+++ src/screens/Counters.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+
+const Counters = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello, World</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Counters;
